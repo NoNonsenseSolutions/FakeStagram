f83996cf3796240ef4161aaf58e3fd0fbdfb9637
Wong You Jing <wongyoujing118@gmail.com>
2016-06-30 16:00:03 +0800

    Initial Commit

---
+++ NewInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewInstagram
+//
+//  Created by Wong You Jing on 30/06/2016.
+//  Copyright © 2016 Nononsense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
