4fd704b8bdf8a6827607b2099c4207127c2f1833
Wong You Jing <wongyoujing118@gmail.com>
2016-07-05 10:48:27 +0800

    complete comment feature

---
+++ NewInstagram/Comment.swift
@@ -0,0 +1,13 @@
+//
+//  Comment.swift
+//  NewInstagram
+//
+//  Created by Wong You Jing on 05/07/2016.
+//  Copyright © 2016 Nononsense. All rights reserved.
+//
+
+import Foundation
+
+class Comment {
+
+}
