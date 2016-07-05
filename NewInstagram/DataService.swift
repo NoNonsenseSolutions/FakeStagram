69f6b40f9edc54240f7d3d6c72a64ac8fe8a6228
Wong You Jing <wongyoujing118@gmail.com>
2016-07-05 10:41:52 +0800

    attempt to create conflict

--- NewInstagram/DataService.swift
+++ NewInstagram/DataService.swift
@@ -12,7 +12,7 @@ import FirebaseDatabase
 class DataService{
   static let dataService = DataService()
   // Added comment for github demo
-  // My hand is too itchy, i should never edit code here
+  // My hand is too itchy, i should never edit code here I am gonna try to make a conflict
   private var _rootRef = FIRDatabase.database().reference()
   private var _currentUserRef = FIRDatabase.database().reference().child("users").child(User.currentUserUid()!)
   private var _imagesRef = FIRDatabase.database().reference().child("images")
